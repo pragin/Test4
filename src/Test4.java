eb66b397368fbf5928378e31c922c260652f6e26
pragin <pragin.martin@hotmail.com>
2021-10-20 19:12:20 +0100

    First push to Test4

---
+++ src/Test4.java
@@ -0,0 +1,8 @@
+public class Test4 {
+        public static void main(String[] args) {
+            for(int i = 0; i<4; i++ ){
+                System.out.println("I Like Git");
+            }
+        }
+    }
+
